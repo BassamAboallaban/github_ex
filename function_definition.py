a01d0f901d6f41a35deda40fb3c4e7fd73d3dae1
BassamAboallaban <107860691+BassamAboallaban@users.noreply.github.com>
2022-06-20 14:15:08 +0100

    Update function_definition.py
    Hi Giulia, just made some changes for the second function. Have a look!

--- function_definition.py
+++ function_definition.py
@@ -5,7 +5,7 @@ def multiply_a_number_by_two(n):
 
 
 # Function that we can use to calculate compound interest
-def compound_interest(base, int_rate, years=10):
+def compound_interest(base, int_rate, years=5):
     multiplier = 1+int_rate
-    final = base * multiplier ** years
+    amount_ci = base * multiplier ** years
     return(final)
